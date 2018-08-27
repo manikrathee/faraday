38d4ce287651d3a9742578b2ef13c65fa3197b60
Manik Rathee <manikrathee@gmail.com>
2018-08-26 20:43:16 -0700

    cleanup and install

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Trend from 'react-trend';
+import { GoogleSheet, GoogleSheetsApi } from '@lourd/react-google-sheet'
+
+
+const weightData = [180,190,180,199,188,200,202,203];
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <Trend data={weightData} />
+        <Trend
+          data={weightData}
+          autoDraw
+          autoDrawDuration={3000}
+          autoDrawEasing="ease-in"
+        />
       </div>
     );
   }
